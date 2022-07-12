0a7b027126555e3aab09ccb2ff3cb4847ec3f463
Mercedes <102251895+MThunder2@users.noreply.github.com>
2022-07-12 00:11:52 -0500

    Add files via upload

--- FinalProblem.java
+++ FinalProblem.java
@@ -1,1 +1,15 @@
-"Week-1-Assignments" 
+
+public class FinalProblem {
+	public static void main(String[] args) {
+		
+		double num1 = 3;
+		double num2 = 3;
+		double num3 = 3;
+		
+		double myDivision = (num1/num2)/num3;
+		
+		System.out.println(myDivision);
+		
+	}
+
+}
---
+++ Problem3.java
@@ -0,0 +1,15 @@
+
+public class Problem3 {
+	public static void main(String[] args) {
+		int num1 = 15;
+		
+		int num2 =4;
+		
+		int finalAnswer = num1%num2;
+		
+		System.out.println(finalAnswer);
+		
+		
+	}
+
+}
