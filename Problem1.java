09e411d094fc1f8b919b17ae82e0f1b56b633ad2
Mercedes <102251895+MThunder2@users.noreply.github.com>
2022-07-12 00:09:12 -0500

    Add files via upload

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,51 @@
-"Week-1-Assignments" 
+import java.util.Scanner;
+
+public class Problem1 {
+	public static void main(String[] args) {
+		double num1 = 10.5;
+		double num2 = .5;
+		
+		double answer1 = num1/num2;
+
+		
+		double num3 = 2;
+		double num4 = 2;
+		
+		double answer2 = num3*num4;
+		
+		double finalAnswer = answer1 + answer2;
+		System.out.println(finalAnswer);
+		
+
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	 
+	 
+	 
+	 
+	 
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,19 @@
-"Week-1-Assignments" 
+public class Problem2 {
+	public static void main(String[] args) {
+		
+		double hourlyRate = 17.52;
+		double taxRate = .25;
+		double hoursWorked = 40;
+		
+		double grossPay = hourlyRate*hoursWorked;
+		
+		double netPay = grossPay - (grossPay*taxRate);
+	
+		
+		System.out.println("Gross Pay: " + grossPay);
+		System.out.println("Net Pay: " + netPay);
+	
+		
+	}
+
+}
